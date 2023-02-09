adc7f19f67c069aa9e003f7d286f02d20f0559e0
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-02-09 13:44:15 +1000

    feat: less prescriptive page layout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -5,5 +5,5 @@ type Props = {
 };
 
 export const PageLayout: React.FC<Props> = ({ children }) => (
-  <div className="w-screen max-w-[100vw]">{children}</div>
+  <div className="bg-white">{children}</div>
 );
